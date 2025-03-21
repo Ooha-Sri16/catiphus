095b5d6e022800bd8031e3bb7bdecd3beb60a8f5
Ooha-Sri16 <oohasriv1116@gmail.com>
2025-03-21 12:43:08 +0530

    Update respose.py

--- respose.py
+++ respose.py
@@ -1,21 +1,13 @@
 import streamlit as st
-import pandas as pd
-from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+name = st.text_input("Enter your name", "John Doe")
+password = st.text_input("Enter your password", type="password")
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
 
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+if st.button("Submit"):
+   if len(password) < 8:
+       st.warning("Password must be at least 8 characters long")
+
+
+   st.success("Password is valid")
